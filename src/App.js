5168e6d2f33759822d69114104fbae2ba2dc4ae5
marwanosman505 <marwanosman505@gmail.com>
2025-06-02 14:42:06 +0100

    added analytics dependency

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 // import Home from './components/Home';
 import Landing from './components/Landing';
 import Main from './components/Main';
+import { Analytics } from '@vercel/analytics/react'
 
 function App() {
   return (
     <div className="App">
       <Main/>
+      <Analytics/>
     </div>
   );
 }
